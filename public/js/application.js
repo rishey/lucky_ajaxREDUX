40010d7f727666c67b4d32f2dfedade33b7fb0b8
richard bluestein <richard@bluestein.com>
2013-09-18 21:33:01 -0500

    first iteration replacing body with response from dom

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
+	$('form'). on('submit', function(event) {
+		event.preventDefault();
+		var roll = Math.floor(Math.random()*6)+1
+		$.post('/rolls',function(data){
+			$("body").html(data);
+		});
+	})
 
   // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- generate a random number between 1 and 6 using JavaScript
-  //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
 });
